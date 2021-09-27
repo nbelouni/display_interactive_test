536557fdb822e09b11547cbca342c794880c4cc8
nbelouni <nbelouni@gmail.com>
2021-09-27 21:45:51 +0200

    csv to array

--- src/Command/AddCommandFromCSVCommand.php
+++ src/Command/AddCommandFromCSVCommand.php
@@ -3,6 +3,7 @@ namespace App\Command;
 
 use App\Service\CommandService;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
@@ -22,19 +23,16 @@ class AddCommandFromCSVCommand extends Command
 	protected function configure(): void
     {
 		$this
+			->addArgument('customers', InputArgument::REQUIRED, 'customers csv file')
+			->addArgument('purchases', InputArgument::REQUIRED, 'purchases csv file')
 			->setDescription('TODO')
 			->setHelp('TODO');
     }
 
 	protected function execute(InputInterface $input, OutputInterface $output)
 	{
-		if (count($input->getArguments()) !== 2) {
-			dump("No requiresd arguments. (see app:add-command --help)");
-			return AddCommandFromCSVCommand::FAILURE;
-		}
-
 		try {
-			$this->commandService->getCommandFromCSV($input->getArguments()[0], $input->getArguments()[1]);
+			$this->commandService->getCommandFromCSV($input->getArgument('customers'), $input->getArgument('purchases'));
 		} catch(\Exception $e) {
 			dump($e->getMessage());
 			return AddCommandFromCSVCommand::FAILURE;
--- src/Service/CommandService.php
+++ src/Service/CommandService.php
@@ -10,16 +10,70 @@ class CommandService
 
 		if (!$customers || !count($customers))
 			throw new \Exception('Enable to find customers.');
-		return $this->getPurchasesFromCSV($customers, $purchasesFilename);
+		$command =  $this->getPurchasesFromCSV($customers, $purchasesFilename);
+		dump($command);
+		//PUT here
 	}
 
-	public function getCustomersFromCSV($customersFilename)
+	private function getCustomersFromCSV($filename)
 	{
-		return null;
-	}
+		if (!($fd = fopen($filename, 'r')))
+			return null;
 
-	public function getPurchasesFromCSV($customers, $purchasesFilename)
-	{
+		$costumers = [];
+		$headers = fgetcsv($fd, 1000, ';');
+		while (($row = fgetcsv($fd, 1000, ';', FILE_SKIP_EMPTY_LINES)) != false)
+		{
+
+			if (array_key_exists(1, $row))
+				$customer['salutation'] = ($row[1] === 1 ? 'mme' : $row[1] === 2) ? 'm' : '';
+			if (array_key_exists(2, $row))
+				$customer['last_name'] = $row[2];
+			if (array_key_exists(3, $row))
+				$customer['first_name'] = $row[3];
+			if (array_key_exists(4, $row))
+				$customer['email'] = $row[4];
+			$customers[$row[0]] = $customer;
+		}
+
+		fclose($fd);
 		return $customers;
 	}
+
+	private function getPurchasesFromCSV($customers, $purchasesFilename)
+	{
+		if (!($fd = fopen($purchasesFilename, 'r')))
+			return null;
+
+		$headers = fgetcsv($fd, 1000, ';');
+		while (($row = fgetcsv($fd, 1000, ';', FILE_SKIP_EMPTY_LINES)) != false)
+		{
+			$ids = null;
+			if (array_key_exists(1, $row))
+				$ids = explode('/', $row[1]);
+			if ($ids)
+			{
+				$customerId = $ids[0];
+				
+				if (array_key_exists(1, $ids))
+					$purchase['product_id'] = $ids[1];
+				if (array_key_exists(3, $row))
+					$purchase['price'] = $row[3];
+				if (array_key_exists(4, $row))
+					$purchase['currency'] = $row[4];
+				if (array_key_exists(2, $row))
+					$purchase['quantity'] = $row[2];
+				if (array_key_exists(5, $row))
+					$purchase['purchased_at'] = $row[5];
+
+				if (array_key_exists($customerId, $customers))
+					$customers[$customerId]['purchases'][] = $purchase;//trim purchase to remove customer id;	
+			}
+		}
+
+		fclose($fd);
+
+		return $customers;
+	}
+
 }
